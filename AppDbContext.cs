591d2f7d83816eb19528876f131f2cbe75e7f02a
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-01 18:04:59 +0300

    Add initial data seeding for products in AppDbContext

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -6,5 +6,18 @@ public class AppDbContext : DbContext
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Product>().HasData(
+        new Product { Id = 1, Name = "Product 1", Price = 10.0M },
+        new Product { Id = 2, Name = "Product 2", Price = 20.0M },
+        new Product { Id = 3, Name = "Product 3", Price = 30.0M }
+        );
+    }
+
+
+
     public DbSet<Product> Products => Set<Product>();
 }
