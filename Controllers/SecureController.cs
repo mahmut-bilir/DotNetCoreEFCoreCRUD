e173d9bd6221b34abb81ddc9762802aba80c337a
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-14 20:16:25 +0300

    Implement JWT authentication and create SecureController for protected endpoints

---
+++ Controllers/SecureController.cs
@@ -0,0 +1,62 @@
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+[ApiController]
+[Route("[controller]")]
+public class SecureController : ControllerBase
+{
+
+    [HttpGet("secure-data")]
+    [Authorize]
+    public IActionResult GetSecureData()
+    {
+        return Ok("This data is for verified users only.");
+    }
+
+
+    [HttpGet("admin")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetAdminData()
+    {
+
+        return Ok("This data is for admin only.");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(string username, string password)
+    {
+
+        // Normally here you should  check the user from the database.
+        if (username == "test" && password == "123")
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ÇokDahaUzunVeGüvenliBirAnahtarBurayaÇokDahaUzunVeGüvenliBirAnahtarBuraya"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+            var token = new JwtSecurityToken(
+
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds
+
+            );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        return Unauthorized();
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using System.Reflection;
 using System.Text;
 using DotNetCoreEFWebApi.Mapping;
 using DotNetCoreEFWebApi.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -18,7 +21,7 @@ builder.Services.AddAuthentication("Bearer")
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes("secret_key_value"))
+            Encoding.UTF8.GetBytes("ÇokDahaUzunVeGüvenliBirAnahtarBurayaÇokDahaUzunVeGüvenliBirAnahtarBuraya"))
     };
 });
 
@@ -28,7 +31,47 @@ builder.Services.AddAuthorization();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
+                      "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                      "Example: \"Bearer 12345abcdef\""
+
+    });
+
+
+
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+     {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+        options.IncludeXmlComments(xmlPath);
+
+});
+
+
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
